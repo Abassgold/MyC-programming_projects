08e040bbd1d0d9496bf40d974285266a7259d023
Abassgold <Abasskola10@gmail.com>
2022-10-12 09:45:52 -0700

    alphabet

---
+++ ifelse/Alphabetcheck.c
@@ -0,0 +1,23 @@
+//Write a program to check if character is alphabet or not
+#include<stdio.h>
+
+int main(void)
+{
+    char c;
+
+    printf("Enter a character");
+    scanf("%c", &c);
+
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        printf("%c is an alphabet.", c);
+  
+
+    }
+    else
+    {
+        printf("%c is not an alphabet", c);
+    }
+
+    return 0;
+}
